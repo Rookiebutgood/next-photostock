d2ee4f3801f609ce5959d0c6b5a3e51ff800a513
Rookiebutgood <demon-01011930@mail.ru>
2020-11-10 00:04:10 +0600

    add unsplash key, add next.config

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,20 +1,25 @@
 import styles from '../styles/Image.module.scss'
 
-export default function Image({src}) {
+export default function Image({img}) {
+  console.log(img)
   return (
     <div className={styles.image}>
-      <img src={src} className={styles.image__bg} />
+      <img src={img.urls.regular} className={styles.image__bg} />
       <div className={styles.image__overlay}>
-        <div className={styles.image__author}>author</div>
+        <div className={styles.image__author}>
+          <img className={styles.image__authorImg} src={img.user.profile_image.small} alt=""/>
+          <span className={styles.image__authorName}>{`${img.user.first_name} ${img.user.last_name}`}</span>
+          <span className={styles.image__authorLink}>{`@${img.user.username}`}</span>
+        </div>
         <div className={styles.image__buttons}>
           <button className={styles.image__button}>
             <img src="./favorite.svg" alt=""/>
           </button>
           <button className={styles.image__button}>
-            <img src="./favorite.svg" alt=""/>
+            <img src="./resize.svg" alt=""/>
           </button>
           <button className={styles.image__button}>
-            <img src="./favorite.svg" alt=""/>
+            <img src="./download.svg" alt=""/>
           </button>
         </div>
       </div>
--- components/ImageList.jsx
+++ components/ImageList.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react'
-import styles from '../styles/ImageList.module.scss'
-import Image from './Image'
+import { useEffect, useState } from 'react';
+import styles from '../styles/ImageList.module.scss';
+import Image from './Image';
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
 
 export default function ImageList() {
   let [outputType, setOutputType] = useState(true)
   let [images, setImages] = useState([])
   useEffect(()=>{
-    fetch('https://picsum.photos/v2/list?page=2&limit=20')
+    fetch('https://api.unsplash.com/photos/random?count=10&client_id='+PUBLIC_KEY,{method:'GET'})
     .then(res=>res.json())
     .then(res=>setImages(prev=>{
       let imageArr = []
       res.forEach((element, index) => {
-        imageArr.push(<Image src={element.download_url} className={styles.imageList__image} key={index} />)
+        imageArr.push(<Image img={element} className={styles.imageList__image} key={index} />)
       });
       return [...prev, imageArr]}))
   }, [])
